4aabd9962f062d31cb3d6e1079fb3fb065b8719d
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-25 11:10:55 -0300

    inicio do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import StateTutorial from "./UseState";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <StateTutorial />
+    </>
+  );
+}
+
+export default App;
