8f3b950122fb4ae171a836822c2e4132593af47e
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-25 14:07:54 -0300

    inicio useMemo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import StateTutorial from "./UseState";
 import ReducerTutorial from "./UseReducer";
 import EffectTutorial from "./UseEffect";
+import MemoTutorial from "./UseMemo";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <EffectTutorial />
+      <MemoTutorial />
     </>
   );
 }
