836d2db8aa8e9b431c9e5ec9b92e900d58dced3e
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-25 13:51:16 -0300

    uso do UseEffect com axios

--- src/UseEffect/index.js
+++ src/UseEffect/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const EffectTutorial = () => {
-  return <div>Hello World</div>;
+  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setData(response.data[0].email);
+        console.log("API WAS CALLED");
+      });
+  }, []);
+
+  return (
+    <div>
+      Hello World
+      <h1>{data}</h1>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
 };
 
 export default EffectTutorial;
