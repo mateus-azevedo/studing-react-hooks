29ce95ad4c0ca51ca2298ad190cd78858d7a05d1
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-25 11:31:56 -0300

    uso basico do useReducer

--- src/UseReducer/index.js
+++ src/UseReducer/index.js
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+    case "toggleShowText":
+      return { count: state.count, showText: !state.showText };
+    default:
+      return state;
+  }
+};
 
 const ReducerTutorial = () => {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(true);
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
 
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button
         onClick={() => {
-          setCount(count + 1);
-          setShowText(!showText);
+          dispatch({ type: "INCREMENT" });
+          dispatch({ type: "toggleShowText" });
         }}
       >
         Click Here
       </button>
 
-      {showText && <p>This is a text</p>}
+      {state.showText && <p>This is a text</p>}
     </div>
   );
 };
