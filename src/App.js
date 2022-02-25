4fc8dc2e6dcdf70906574a1f27bbc0414d3ae41c
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-25 11:56:02 -0300

    inicio UseEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import StateTutorial from "./UseState";
 import ReducerTutorial from "./UseReducer";
+import EffectTutorial from "./UseEffect";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <ReducerTutorial />
+      <EffectTutorial />
     </>
   );
 }
---
+++ src/UseEffect/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EffectTutorial = () => {
+  return <div>Hello World</div>;
+};
+
+export default EffectTutorial;
